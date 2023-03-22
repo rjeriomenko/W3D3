7bb829c9a9a3290b4d79a63e472cc0e0776d6093
rokasj <rokas@jerio.me>
2023-03-22 12:18:19 -0400

    Add rec_sum, start iter_sum

--- practice-for-ch-ruby-recursion-long-practice-main/work.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/work.rb
@@ -4,3 +4,15 @@ def range(start, last)
 
     range(start + 1, last).unshift(start)
 end
+
+def rec_sum(arr)
+    return 0 if arr.length == 0
+    return arr[0] if arr.length == 1
+
+    sum = arr[0]
+    sum += rec_sum(arr[1..])
+end
+
+def iter_sum(arr)
+    #code
+end
