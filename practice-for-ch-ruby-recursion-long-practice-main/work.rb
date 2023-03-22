6494120f52b711e701c224adc6ccabaf2ca25ae0
rokasj <rokas@jerio.me>
2023-03-22 14:24:34 -0400

    Work on dup

--- practice-for-ch-ruby-recursion-long-practice-main/work.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/work.rb
@@ -34,6 +34,31 @@ def exp_2(b, n)
     else
         b * exp_2(b, (n - 1) /2) ** 2
     end
-
-
 end
+
+class Array
+    def dup
+        return [] if self.length == 0
+        return [self[0]] if self.length == 1
+
+        array = []
+        
+        array += [self[0]]
+        array += self[1..].dup
+
+        array
+    end
+end
+
+x = [1,[2]]
+y = x.dup
+p x
+p y
+p x.object_id
+p y.object_id
+p x[1]
+p x[1].object_id
+p y[1].object_id
+y[1] << 3
+p x
+
