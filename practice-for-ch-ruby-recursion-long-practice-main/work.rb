a43ea232775d94d0979383a9923e1fab31d8d888
Ethanjonm <ethanjonmercado@gmail.com>
2023-03-22 17:32:25 -0400

    merge

--- practice-for-ch-ruby-recursion-long-practice-main/work.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/work.rb
@@ -93,14 +93,13 @@ def bsearch(arr, target)
     end
 end
 
-p bsearch([1, 2, 3], 1) # => 0
-p bsearch([2, 3, 4, 5], 3) # => 1
-p bsearch([2, 4, 6, 8, 10], 6) # => 2
-p bsearch([1, 3, 4, 5, 9], 5) # => 3
-p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+def merge_sort(arr)
+    return arr if arr.length < 2
 
+    
+
+
+end
 
 
 
